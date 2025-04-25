6c9b8e7b763799a7edd1918b3fc5cf719a8dd3f3
PTarunakumar <parthtarun@yahoo.co.uk>
2025-04-25 14:34:30 +0100

    Refactor StatisticsGUI to generate elements that was formerly created by UI Designer

--- Part2/StatisticsGUI.java
+++ Part2/StatisticsGUI.java
@@ -1,23 +1,65 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class StatisticsGUI {
+    private Race race;
     private JFrame frame = new JFrame();
     private JPanel statisticsPanel;
-    private JPanel horseDetailsContainer;
-    private JLabel raceParticipationCountTextField;
+    private JPanel horseDetailsPanel;
+    private JLabel noOfRacesTextField;
     private JLabel noOfWinsTextField;
     private JLabel noOfWinsLabel;
-    private JLabel raceParticipationCountLabel;
+    private JLabel noOfRacesLabel;
     private JComboBox<Horse> horseSelectorComboBox;
-    private JPanel horseSelectorContainer;
+    private JPanel horseSelectorPanel;
     private JTextArea historyTextField;
     private JLabel averageSpeedLabel;
     private JLabel averageSpeedTextField;
+    private JPanel titlePanel;
+    private JLabel statisticsLabel;
+
+    void setUI(Race race)
+    {
+
+    }
 
     StatisticsGUI(Race race)
     {
+        this.race = race;
+
+
+        //Statistics Panel
+        statisticsPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints statisticsPanelGbc = new GridBagConstraints();
+        statisticsPanelGbc.insets = new Insets(5, 30, 5, 30);
+        statisticsPanelGbc.fill = GridBagConstraints.HORIZONTAL;
+
+        // Title Panel
+        titlePanel = new JPanel(new GridBagLayout());
+        GridBagConstraints titlePanelGbc = new GridBagConstraints();
+
+        // Add statisticsLabel to titlePanel
+        statisticsLabel = new JLabel("Statistics");
+        statisticsLabel.setFont(new Font("Arial", Font.BOLD, 24));
+        titlePanelGbc.gridx = 0;
+        titlePanelGbc.gridy = 0;
+        titlePanel.add(statisticsLabel, titlePanelGbc);
+
+        // Add titlePanel to statisticsPanel
+        statisticsPanelGbc.gridx = 0;
+        statisticsPanelGbc.gridy = 0;
+        statisticsPanel.add(titlePanel, statisticsPanelGbc);
+
+        // Horse Selector Panel
+        horseSelectorPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints horseSelectorPanelGbc = new GridBagConstraints();
+        horseSelectorPanelGbc.gridx = 0;
+        horseSelectorPanelGbc.gridy = 0;
+
+        // Add horseSelectorComboBox to horseSelectorPanel
+        horseSelectorComboBox = new JComboBox<>();
         horseSelectorComboBox.setRenderer(new DefaultListCellRenderer() {
             @Override
             public java.awt.Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean hasFocus) {
@@ -29,19 +71,73 @@ public class StatisticsGUI {
                 return label;
             }
         });
-
         horseSelectorComboBox.addItem(null);
         for (Horse horse : race.getHorses())
         {
             horseSelectorComboBox.addItem(horse);
         }
+        horseSelectorPanel.add(horseSelectorComboBox, horseSelectorPanelGbc);
+
+        // Add horseSelectorPanel to statisticsPanel
+        statisticsPanelGbc.gridx = 0;
+        statisticsPanelGbc.gridy = 1;
+        statisticsPanel.add(horseSelectorPanel, statisticsPanelGbc);
+
+        // Horse Details Panel
+        horseDetailsPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints horseDetailsPanelGbc = new GridBagConstraints();
+
+        //add noOfRacesLabel and textField to horseDetailsPanel
+        noOfRacesLabel = new JLabel("No of Races:");
+        horseDetailsPanelGbc.gridx = 0;
+        horseDetailsPanelGbc.gridy = 0;
+        horseDetailsPanel.add(noOfRacesLabel, horseDetailsPanelGbc);
+
+        noOfRacesTextField = new JLabel("Races");
+        horseDetailsPanelGbc.gridx = 1;
+        horseDetailsPanel.add(noOfRacesTextField, horseDetailsPanelGbc);
+
+        //add noOfWinsLabel and textField to horseDetails Panel
+        noOfWinsLabel = new JLabel("No of wins:");
+        horseDetailsPanelGbc.gridx = 0;
+        horseDetailsPanelGbc.gridy = 1;
+        horseDetailsPanel.add(noOfWinsLabel, horseDetailsPanelGbc);
+
+        noOfWinsTextField = new JLabel("Wins");
+        horseDetailsPanelGbc.gridx = 1;
+        horseDetailsPanelGbc.gridy = 1;
+        horseDetailsPanel.add(noOfWinsTextField, horseDetailsPanelGbc);
+
+        //add averageSpeedLabel and textField to horseDetailsPanel
+        averageSpeedLabel = new JLabel("Average Speed:");
+        horseDetailsPanelGbc.gridx = 0;
+        horseDetailsPanelGbc.gridy = 2;
+        horseDetailsPanel.add(averageSpeedLabel, horseDetailsPanelGbc);
+
+        averageSpeedTextField = new JLabel("Speed");
+        horseDetailsPanelGbc.gridx = 1;
+        horseDetailsPanelGbc.gridy = 2;
+        horseDetailsPanel.add(averageSpeedTextField, horseDetailsPanelGbc);
+
+        // add horseDetailsPanel to statisticsPanel
+        statisticsPanelGbc.gridx = 0;
+        statisticsPanelGbc.gridy = 2;
+        statisticsPanel.add(horseDetailsPanel, statisticsPanelGbc);
+
+        //add statsHistory to statisticsPanel;
+        historyTextField = new JTextArea("Stats");
+        statisticsPanelGbc.gridx = 0;
+        statisticsPanelGbc.gridy = 3;
+        statisticsPanel.add(historyTextField,statisticsPanelGbc);
+
+        //Listeners
 
         horseSelectorComboBox.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Horse selectedHorse = (Horse) horseSelectorComboBox.getSelectedItem();
                 if (selectedHorse != null) {
-                    raceParticipationCountTextField.setText(String.valueOf(selectedHorse.getTotalRaces()));
+                    noOfRacesTextField.setText(String.valueOf(selectedHorse.getTotalRaces()));
                     noOfWinsTextField.setText(String.valueOf(selectedHorse.getTotalWins()));
                     averageSpeedTextField.setText(String.format("%.2f", calculateSpeed(selectedHorse)));
 
